7df9d173cc457eceba0daebce9c52f72b7fb8c69
Chen Zeyu <zeyufly@gmail.com>
2013-03-21 01:42:15 +0800

    add sever side

--- peerMap.js
+++ peerMap.js
@@ -1,4 +1,6 @@
 var map;
+var ip;
+var allMarkers = [];
 function initialize() {
   var mapOptions = {
     zoom: 8,
@@ -8,21 +10,62 @@ function initialize() {
   if(navigator.geolocation){//html5 location supported
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      $.getJSON("http://jsonip.appspot.com?callback=?", function(data){
+        $.ajax({
+          url: "webInsertLocation.php",
+          type: "POST",
+          data: {
+            'ip' : data.ip,
+            'long': pos.lng(),
+            'lat' : pos.lat()
+          },
+        }).done(function(){
+          ip = data.ip;
+          console.log("inserted! cool!");
+        });
+      });
+      $.ajax({
+        url: "webGetAll.php",
+        type: "GET",
+        dataType: "json"
+      }).done(function(data){
+        addMarker(data);
+      });
+      setInterval(function(){
+        console.log("update markers");
+        $.ajax({
+          url: "webGetAll.php",
+          type: "GET",
+          dataType: "json"
+        }).done(function(data){
+          addMarker(data);
+        });
+      },3000);
       map.setCenter(pos);
-   var marker = new google.maps.Marker({
-     map: map,
-     position: map.getCenter()
-   });
-   var newpos = new google.maps.LatLng(-25.363882,131.044922);
-   var marker = new google.maps.Marker({
-     map: map,
-     position: newpos 
-   });
-    }, function(){
-      console.log("error getting location");
     });
   } else{
-      alert("html5 geolocation is not supported by your browser");
+    alert("html5 geolocation is not supported by your browser");
+  }
+}
+function addMarker(data){
+  for(var i = 0; i < allMarkers.length; i++){
+    allMarkers[i].setMap(null);
+  }
+  for(var i = 0; i < data.length; i++){
+    var object = data[i];
+    var pos = new google.maps.LatLng(object.latitude, object.longitude);
+    var marker = new google.maps.Marker({
+      map: map,
+      position:pos
+    });
+    allMarkers.push(marker);
   }
 }
 $(document).ready(initialize);
+$(window).on('beforeunload',function (){
+  $.ajax({
+    url: "webDeleteLocation.php",
+    type: "POST",
+    data: {'ipaddr': ip.toString()}
+  });
+});
