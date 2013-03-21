ddbf786dd3a705f970ec8e07cbb0ff33378a1cd9
AstroProfundis <github@mail.atr.me>
2013-03-21 16:18:04 +0800

    Fixed: use placeholder in database setting

--- webDeleteLocation.php
+++ webDeleteLocation.php
@@ -1,6 +1,7 @@
 <?php
 $key = $_POST['ipaddr'];
-$db = new mysqli('localhost','root','passwd','peerMapWeb');
+// Replace with your own database infomation
+$db = new mysqli('db_host','db_username','db_passwd','db_name');
 echo "console.log($key)";
 if($db->connect_errno){
         die('Unable to connect to database [' . $db->connect_error . ']');
